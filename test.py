ed86ea12fda4a04dcdab57d93a1c272e4a16256d
ericdennis7 <ericdennis11@gmail.com>
2025-05-07 19:22:58 +0000

    Minor improvements

--- test.py
+++
@@ -1,7 +0,0 @@
-import ipinfo
-access_token = 'f805893e85f4ea'
-handler = ipinfo.getHandler(access_token)
-ip_address = '216.239.36.21'
-details = handler.getDetails(ip_address)
-details.city
-details.loc
